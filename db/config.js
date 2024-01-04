e9bc45e93522f11beb227090b52231ebd2ff1b56
Akash Raj <134199779+Akash77312@users.noreply.github.com>
2024-01-04 18:13:03 +0530

    Update config.js

--- db/config.js
+++ db/config.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/e-commerce");
-
+// mongoose.connect("mongodb://localhost:27017/e-commerce");
+mongoose.connect("mongodb+srv://akash:akash@123@cluster0.mvts9o1.mongodb.net/e-commerce");
